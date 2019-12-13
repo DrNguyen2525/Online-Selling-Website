4c9e2a323d0b66ede68f865607d45bc528433315
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2019-12-13 16:19:39 +0700

    add: microservices' URLs

---
+++ service_explorer.py
@@ -0,0 +1,11 @@
+payment_service = "https://shopsp01.herokuapp.com"
+account_service = "https://secure-mountain-93147.herokuapp.com"
+product_service = "https://nguyenvd27-ltct-demo.herokuapp.com"
+order_service = "https://sp-04-order.herokuapp.com"
+banner_coupon_service = "https://secure-mesa-29267.herokuapp.com"
+
+configuration_service = "http://52.87.98.116"
+customer_care_service = "https://infinite-chamber-68193.herokuapp.com"
+search_service = "http://laptrinhcautruc.herokuapp.com"
+delivery_service = "https://online-selling-website.herokuapp.com"
+logistic_service = "https://m11-inventory.herokuapp.com"
